01c0ba54fee5cdd065463fe8d4397950b009b2dc
Jordan Hudgens <jordan@devcamp.com>
2017-06-20 17:09:46 +0000

    implemented edit and update functionality

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -6,4 +6,22 @@ class PortfoliosController < ApplicationController
   def new
     @portfolio_items=Portfolio.new
   end
+  
+   def create
+    @portfolio_item = Blog.new(params.require(:portfolio).permit(:title, :subtitle, :body))
+
+    respond_to do |format|
+      if @portfolio_item.save
+        format.html { redirect_to portfolios_path, notice: 'your portfoli item is alive.' }
+
+      else
+        format.html { render :new }
+ 
+      end
+    end
+  end
+  
+  def edit
+    @portfolio = Portfolio.find(params(:id))
+  end
 end
